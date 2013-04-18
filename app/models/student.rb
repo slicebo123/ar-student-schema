1639ce699aa5e386150b8a2e2dd9af969abd9050
Apprentice <apprentice@dbc32.local>
2013-04-18 15:17:30 -0700

    Your First Object-Relational Model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,27 @@
 require_relative '../../db/config'
 
-# implement your Student model here
+class Student
+  attr_reader :birthday
+  def assign_attributes(args)
+    @id = args[:id]
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @gender = args[:gender]
+    @email = args[:email]
+    @phone = args[:phone]
+    @birthday = args[:birthday]
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def age
+    now = Date.today
+    age = now.year - @birthday.year - ((now.month > @birthday.month || (now.month == @birthday.month && now.day >= @birthday.day)) ? 0 : 1)
+    age
+  end
+
+  def self.delete_all
+  end
+end
